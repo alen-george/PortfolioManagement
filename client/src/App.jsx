107ddf26ad2912e7536260aa708f0dc94c4eb2b6
Zaintol <zainibrahem5@gmail.com>
2025-06-28 14:25:36 +0400

    Adding landing page to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import AppRoutes from "./Routes/route";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: { main: '#1976d2' },
+    secondary: { main: '#9c27b0' },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
+    
   );
 }
 
--- client/src/Routes/routeConfig.jsx
+++ client/src/Routes/routeConfig.jsx
@@ -1,8 +1,14 @@
 import { lazy } from 'react';
 
 const PortfolioPerformance = lazy(() => import('../Portfolio/pages/PortfolioPerformance'));
-
+const LandingPage = lazy(() => import('../pages/LandingPage'));
 export const routes = [
+  
+  {
+    path: '/',
+    element: <LandingPage />,
+    layout: 'main'
+  },
   {
     path: '/portfolio/performance',
     element: <PortfolioPerformance />,
